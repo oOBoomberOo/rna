b63e98f03aacbfbd95e72dd5c1f24642b59ef618
Boomber <boomberisalreadytaken@gmail.com>
2020-02-15 16:26:31 +0700

    Fix documentation to use the correct path

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 //! Loot Table Script is created by [Minecraft Datapacks](https://discord.gg/56ySADc) community to create more robust loot table syntax.
 //! # Examples
 //! ```
-//! let loot_a = rna::interpret_file("test/loot_a.megu").unwrap();
-//! let loot_b = rna::interpret_file("test/loot_b.megu").unwrap();
+//! use rna::utils;
 //! 
-//! let merged_loot = rna::merge(&[loot_a, loot_b]).unwrap();
+//! let loot_a = utils::interpret_file("test/loot_a.megu").unwrap();
+//! let loot_b = utils::interpret_file("test/loot_b.megu").unwrap();
+//! 
+//! let merged_loot = utils::merge(&[loot_a, loot_b]).unwrap();
 //! ```
 
 mod megu;
